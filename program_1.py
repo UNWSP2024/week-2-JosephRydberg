d16a5b00611dcfde6cf271d1f9786aefb006e456
JosephRydberg <jwrydberg@students.unwsp.edu>
2024-09-09 13:25:50 -0500

    Completed Assignment

--- program_1.py
+++ program_1.py
@@ -1,5 +1,9 @@
+#Joseph Rydberg, 9/9/2024, Info
 def personal_information():
-    print('...')
+    print("Joseph Rydberg")
+    print("10692, 38th cir NE, 55376")
+    print("(612)803-1539")
+    print("No Major")
 
 
 # Line which calls the above function.
--- program_3.py
+++ program_3.py
@@ -1,8 +1,16 @@
+#Joseph Rydberg, 9/9/2024, ItemCost
 def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+    i1 = int(input("enter item 1 cost"))
+    i2 = int(input("enter item 2 cost"))
+    i3 = int(input("enter item 2 cost"))
+    i4 = int(input("enter item 4 cost"))
+    i5 = int(input("enter item 5 cost"))
+
+    total = (i1 + i1 + i3 + i4 + i5)
+    print(total)
+    totalTax = (i1 + i1 + i3 + i4 + i5) * .07
+    print(totalTax)
+
+    print(total + totalTax)
 
 calculate_total_purchase()
